1b52ea46fd0e9fb847f3ab29101d779dc3f65c3f
tmelanson <tmelanson17@gmail.com>
2018-06-25 17:27:57 -0400

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tj/Projects/ROSPlayground/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/tj/Projects/ROSPlayground/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/lunar/bin:/home/tj/bin:/home/tj/.local/bin:/usr/local/cuda-8.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda-8.0/bin:/snap/bin"
+export PKG_CONFIG_PATH="/home/tj/Projects/ROSPlayground/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/tj/Projects/ROSPlayground/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tj/Projects/ROSPlayground/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/tj/Projects/ROSPlayground/catkin_ws/src:$ROS_PACKAGE_PATH"
